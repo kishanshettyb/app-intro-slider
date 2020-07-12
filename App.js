66a343883913c6aecd1adfd9548e5313ac9b52bf
kishanshettyb <kishanqr@gmail.com>
2020-07-12 14:05:38 +0530

    refactoring app file

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 import WelcomeScreen from "./src/screens/WelcomeScreen";
-import LoginScreen from "./src/screens/LoginScreen";
 
 export default function App() {
   return (
